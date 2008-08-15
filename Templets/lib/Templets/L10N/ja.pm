c67e102dd9a4cb786e069b983ebe0cae8103cb06
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2008-08-15 15:30:20 +0000

    A little fix for description.
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-Templets/trunk@3 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- Templets/lib/Templets/L10N/ja.pm
+++ Templets/lib/Templets/L10N/ja.pm
@@ -6,10 +6,11 @@ use base qw( Templets::L10N );
 use vars qw( %Lexicon );
 
 our %Lexicon = (
-    qq{Templets allows you to handle a templet, which means partial template set.} => qq{Templetsを使うと、テンプレット(部分テンプレートセット)を扱うことができるようになります。},
+    qq{Templets plugin provides a framework for making Template Sets pluggable.}
+      => qq{Templetsプラグインは、テンプレートセットをpluggableにするためのフレームワークを提供します。},
     'Append a templet' => 'テンプレットを追加',
     'Select a templet' => 'テンプレットの選択',
-    'Append' => '追加',
+    'Append'           => '追加',
 );
 
 1;
